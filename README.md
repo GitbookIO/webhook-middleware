dede2bcedfca0cb842497c2ef9b7861f354f6344
Samy Pessé <samypesse@gmail.com>
2015-11-04 12:02:10 +0100

    Base code

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# webhook-middleware
-Middleware to verify GitBook webhooks
+# gitbook-webhook-middleware
+
+Express middleware to verify GitBook webhooks.
+
+### Installation
+
+```
+npm install gitbook-webhook-middleware
+```
+
+### Usage
+
+```js
+var express = require('express');
+var app     = express();
+var gitbookMiddleware = require('gitbook-webhook-middleware')({
+    secret: process.env.GITBOOK_SECRET
+});
+
+app.post('/hooks/gitbook/', gitbookMiddleware, function(req, res) {
+    // Only respond to gitbook publish events
+    if (req.headers['x-gitbook-event'] != 'publish') return res.status(200).end();
+
+    var payload = req.body;
+
+    // Do something
+});
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+var bodyParser = require('body-parser');
+var crypto = require('crypto');
+
+function signBlob(key, blob) {
+    return 'sha1=' + crypto.createHmac('sha1', key).update(blob).digest('hex');
+}
+
+module.exports = function(options) {
+    options = options || { secret: '' };
+
+    if (typeof options.secret != 'string' || options.secret === '')
+        throw new TypeError('must provide a \'secret\' option');
+
+    return bodyParser.json({
+        verify: function(req, res, buffer) {
+            if (!req.headers['x-gitbook-signature'])
+                throw new Error('No X-GitBook-Signature found on request');
+
+            if (!req.headers['x-gitbook-event'])
+                throw new Error('No X-GitBook-Event found on request');
+
+            if (!req.headers['x-gitbook-delivery'])
+                throw new Error('No X-GitBook-Delivery found on request');
+
+            var received_sig = req.headers['x-gitbook-signature'];
+            var computed_sig = signBlob(options.secret, buffer);
+
+            if (received_sig != computed_sig) {
+                throw new Error('Invalid Signature');
+            }
+        }
+    });
+};
